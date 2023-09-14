897d4a9c66af3e34a791a5a96a6a6cfe89d1a5a5
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-14 10:46:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
